0c1fd0dc0117e8d5d4d359308a85f908472f8b5c
Aleksandar Danilovic <sasadanilovic@yahoo.com>
2021-11-17 00:14:37 +0100

    Better formating

--- lambdas/Solution.java
+++ lambdas/Solution.java
@@ -5,9 +5,9 @@ public class Solution {
 	
     public int numDifferentIntegers(String word) {
 		return Arrays.stream(word.replaceAll("[a-z]+", " ").split(" "))
-                .filter(s -> !"".equals(s))
-                .map(s -> s.replaceFirst("^0+", ""))
-			    .collect(Collectors.toSet())
-                .size();
+                     .filter(s -> !"".equals(s))
+                     .map(s -> s.replaceFirst("^0+", ""))
+                     .collect(Collectors.toSet())
+                     .size();
     }
 }
